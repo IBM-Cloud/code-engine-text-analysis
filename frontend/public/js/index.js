3eee24f6c2e318e4f28f4e94b72371dc9c29233e
Vidyasagar Machupalli <vidyasagarmsc@gmail.com>
2020-04-15 17:59:13 +0530

    Updated frontend index.js

--- frontend/public/js/index.js
+++ frontend/public/js/index.js
@@ -1,53 +1,53 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $("p.error").text("");
-   $("p.success").text("");
+  $("p.success").text("");
   $("#uploadbtn").attr("disabled", true);
-  $("#file").change(function() {
+  $("#file").change(function () {
     showUploadedImage(this);
     $("#uploadbtn").removeAttr("disabled");
   });
-  $("#imageForm").submit(function() {
+  $("#imageForm").submit(function () {
     $("#uploadbtn").addClass("is-loading");
     $("#table tbody tr").remove();
     $(this).ajaxSubmit({
-      error: function(data) {
+      error: function (data) {
         $("p.error").text(data);
       },
-      success: function(response) {
+      success: function (response) {
         $("#uploadbtn").removeClass("is-loading");
         $("#uploadbtn").attr("disabled", true);
         console.log(response);
         $("p.success").text(response.data);
-      }
+      },
     });
     return false;
   });
   // Check for click events on the navbar burger icon
-  $(".navbar-burger").click(function() {
-
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
-
+  $(".navbar-burger").click(function () {
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
   });
 
-
-// Shows the preview of uploaded image
-function showUploadedImage(fileInput) {
-  var uploadbtn = document.getElementById("uploadbtn");
-  var files = fileInput.files;
-  if (files.length > 0) {
-    for (var i = 0; i < files.length; i++) {
-      var file = files[i];
-      var imageType = /image.*/;
-      if (!file.type.match(imageType)) {
-        continue;
-      }
-      $("#column-multiline").append('<div class="column is-one-half-desktop is-half-tablet">\
+  // Shows the preview of uploaded image
+  function showUploadedImage(fileInput) {
+    var uploadbtn = document.getElementById("uploadbtn");
+    var files = fileInput.files;
+    if (files.length > 0) {
+      for (var i = 0; i < files.length; i++) {
+        var file = files[i];
+        var imageType = /image.*/;
+        if (!file.type.match(imageType)) {
+          continue;
+        }
+        $("#column-multiline").append(
+          '<div class="column is-one-half-desktop is-half-tablet">\
                   <div class="card">\
                       <div class="card-image">\
                           <figure class="image is-3by2">\
-                            <img id="uploadedimg'+i+'" src="https://unsplash.it/300/200/?random&pic=1" alt="placeholder">\
+                            <img id="uploadedimg' +
+            i +
+            '" src="https://unsplash.it/300/200/?random&pic=1" alt="placeholder">\
                           </figure>\
                           <div class="card-content is-overlay">\
                             <span class="tag is-info is-pulled-right">\
@@ -61,19 +61,20 @@ function showUploadedImage(fileInput) {
                           </a>\
                       </footer>\
                   </div>\
-                </div>');
-      //const fileName = document.querySelector("#file-js-example .file-name");
-      //fileName.textContent = files[i].name;
-      var img = document.getElementById("uploadedimg"+i);
-      img.file = file;
-      var reader = new FileReader();
-      reader.onload = (function(aImg) {
-        return function(e) {
-          aImg.src = e.target.result;
-        };
-      })(img);
-      reader.readAsDataURL(file);
+                </div>'
+        );
+        //const fileName = document.querySelector("#file-js-example .file-name");
+        //files[i].name = 'image_'+i-1+file.type;
+        var img = document.getElementById("uploadedimg" + i);
+        img.file = file;
+        var reader = new FileReader();
+        reader.onload = (function (aImg) {
+          return function (e) {
+            aImg.src = e.target.result;
+          };
+        })(img);
+        reader.readAsDataURL(file);
+      }
     }
   }
-}
 });
