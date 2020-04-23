a59b6caad50e61dd65227d674bc9f8c367e60387
Vidyasagar Machupalli <vidyasagarmsc@gmail.com>
2020-04-23 17:38:45 +0530

    Updated the backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 "use strict";
-const myCOS = require('ibm-cos-sdk');
-const multer = require('multer');
-const multerS3 = require('multer-s3');
+const myCOS = require("ibm-cos-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -12,52 +12,71 @@ require("dotenv").config({
 const cors = require("cors");
 app.use(cors());
 
-var config = {
-    endpoint: process.env.COS_ENDPOINT || 's3.us-south.cloud-object-storage.appdomain.cloud',
-    apiKeyId: process.env.COS_APIKEY,
-    ibmAuthEndpoint: 'https://iam.cloud.ibm.com/identity/token',
-    serviceInstanceId: process.env.COS_RESOURCE_INSTANCE_ID
+const port = process.env.PORT || 3001;
+
+function uploadFilesToCOS(req, res, next) {
+  var config = {
+  endpoint:
+    process.env.COS_ENDPOINT ||
+    "s3.us-south.cloud-object-storage.appdomain.cloud",
+  apiKeyId: process.env.COS_APIKEY,
+  ibmAuthEndpoint: "https://iam.cloud.ibm.com/identity/token",
+  serviceInstanceId: process.env.COS_RESOURCE_INSTANCE_ID,
 };
 
 var cosClient = new myCOS.S3(config);
-const port = process.env.PORT || 3001;
 
 var upload = multer({
   storage: multerS3({
     s3: cosClient,
     bucket: process.env.COS_BUCKETNAME+'/images',
     metadata: function (req, file, cb) {
-      cb(null, {fieldName: file.fieldname});
+      cb(null, { fieldName: file.fieldname });
     },
     key: function (req, file, cb) {
-      cb(null, file.originalname)
+      cb(null, file.originalname);
+    },
+  }),
+}).array("files", 10);
+
+  upload(req, res, function (err) {
+    if (err) {
+      next(err);
     }
-  })
-});
+    if (req.files.length > 1) {
+      res.send(
+        "Successfully uploaded " + req.files.length + " files to Object Storage"
+      );
+    }
+    else{
+    res.send(
+      "Successfully uploaded " + req.files.length + " file to Object Storage"
+    );
+    }
+  });
+}
 
 /*
  * Default route for the web app
  */
-app.get('/', function(req, res) {
-    res.send("Hello World! from backend");
+app.get("/", function (req, res) {
+  res.send("Hello World! from backend");
 });
 /*
  * Upload an image for object detection
  */
-app.post('/images', upload.array('files', 10), function(req, res, next) {
-  if(req.files.length > 1){
-   res.send('Successfully uploaded ' + req.files.length + ' files to Object Storage');
-  }
-  res.send('Successfully uploaded ' + req.files.length + ' file to Object Storage');
+app.post("/images", uploadFilesToCOS, function(req, res, next) {
+  next();
 });
 
-app.all("*", function(req,res,next) {
+app.all("*", function (req, res, next) {
   var err = new Error("Route not supported. Please check your backend URL");
-   next(err);
+  next(err);
 });
 
-app.use(function(error, req, res, next) {
-  res.status(500).send(error.message);
+app.use(function (error, req, res, next) {
+  console.log(error);
+  res.status(500).send("Oops!! An error occurred. Check the logs for more info. This is a message from your BACKEND");
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
